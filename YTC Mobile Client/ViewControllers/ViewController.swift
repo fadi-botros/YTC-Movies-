017d397e015a90313bd59848c2f5e7e33e7df86d
Mohammed Oshaiba <roooney_2010@outlook.com>
2017-12-16 16:08:36 +0200

    intial commit

---
+++ YTC Mobile Client/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YTC Mobile Client
+//
+//  Created by apple on 12/16/17.
+//  Copyright © 2017 sharpmobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
